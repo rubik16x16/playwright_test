065154511242212adc07ed7f44b19d960c46055e
anthony.gonzalez <anthony.gonzalez@alphas.technology>
2025-04-05 03:54:55 +0000

    chore: click dates range

--- playwright_test.py
+++ playwright_test.py
@@ -1,30 +1,96 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 options = webdriver.ChromeOptions()
-options.add_argument('--headless')  # Run Chrome in headless mode
-options.add_argument('--no-sandbox')  # Bypass OS security model
-options.add_argument(
-	'--disable-dev-shm-usage'
-)  # Overcome limited resource problems
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
 
 service = Service('/app/chromedriver-linux64/chromedriver')
 
 driver = webdriver.Chrome(service=service, options=options)
+driver.set_window_size(1920, 1080)
+
+TIMEOUT = 90
 
 try:
-	# Open a webpage
-	driver.get("https://example.com")
+	driver.get('https://www.hotel-barcelonahouse.com/')
 
-	# Get the title of the page
 	title = driver.title
-	print("Page Title:", title)
+	print('Page Title:', title)
 
-	# Locate an element (for demonstration, finding the <h1> element)
-	element = driver.find_element(By.TAG_NAME, "h1")
-	print("Heading:", element.text)
+	element = driver.find_element(By.TAG_NAME, 'h1')
+	print('Heading:', element.text)
+
+	booking_button = driver.find_element(
+		By.CSS_SELECTOR,
+		'.roi-search-engine__field.roi-search-engine__field--action'
+	)
+
+	ActionChains(driver).move_to_element(booking_button).click().perform()
+
+	calendar = WebDriverWait(driver, TIMEOUT).until(
+		EC.presence_of_element_located((By.CSS_SELECTOR, '.nd-calendario'))
+	)
+
+	calendar_first_frame = calendar.find_elements(
+		By.CSS_SELECTOR, '.calendar.js-calendar'
+	)[0].find_element(By.CSS_SELECTOR, '.calendar-days')
+
+	calendar_days = WebDriverWait(calendar_first_frame, TIMEOUT).until(
+		EC.presence_of_all_elements_located(
+			(
+				By.CSS_SELECTOR,
+				'.calendar-day.calendar-day-number.day-available'
+			)
+		)
+	)
+
+	day_from = calendar_days[0]
+
+	ActionChains(driver).move_to_element(day_from).click(day_from).perform()
+
+	action_btns = driver.find_element(
+		By.CSS_SELECTOR, '#calendario-nodispo-botones-acciones'
+	)
+
+	change_days_btn = action_btns.find_element(
+		By.CSS_SELECTOR, '.return-checkin.js-nodispo-return-checkin'
+	)
+
+	WebDriverWait(driver, 90).until(
+		EC.invisibility_of_element_located(
+			(By.CSS_SELECTOR, '#calendar-overlay')
+		)
+	)
+
+	driver.get_screenshot_as_file('/app/screenshots/success/foo.png')
+
+	calendar_days = WebDriverWait(calendar_first_frame, TIMEOUT).until(
+		EC.presence_of_all_elements_located(
+			(
+				By.CSS_SELECTOR,
+				'.calendar-day.calendar-day-number.day-available'
+			)
+		)
+	)
+
+	day_to = calendar_days[0]
+
+	ActionChains(driver).move_to_element(day_to).click(day_to).perform()
+
+	WebDriverWait(action_btns, TIMEOUT).until(
+		EC.visibility_of_element_located(
+			(By.CSS_SELECTOR, '.send-form.spinner.js-nodispo-book-nodispo')
+		)
+	)
+
+	driver.get_screenshot_as_file('/app/screenshots/success/bar.png')
 
 finally:
-	# Close the browser
+	driver.get_screenshot_as_file('/app/screenshots/error/foo.png')
 	driver.quit()
